d2fe86613b2bf43e8ded36925dfc7f453ef2c962
JimGreenberg <greenjberg@gmail.com>
2017-03-22 18:38:07 -0400

    change ajax to getJSON

--- frontend/util/weather_api_util.js
+++ frontend/util/weather_api_util.js
@@ -2,14 +2,14 @@ const apiKey = '0c6887ca19ca753c65c470a739cbd8c6'; // please don't steal
 // const apiKey =  'f816d7f39052e3a98b21952097a43076';
 export const fetchWeather = query => {
   const type = !!parseInt(query) ? 'id' : 'q';
-  return $.ajax({
+  return $.getJSON({
     method: "GET",
     url: `https://api.openweathermap.org/data/2.5/weather?${type}=${query}&APPID=${apiKey}&units=imperial`
   });
 };
 
 export const fetchBatchWeather = query => {
-  return $.ajax({
+  return $.getJSON({
     method: "GET",
     url: `https://api.openweathermap.org/data/2.5/group?id=${query}&APPID=${apiKey}&units=imperial`
   });
