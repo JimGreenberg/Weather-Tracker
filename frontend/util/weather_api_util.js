089f87d5bb22ac0576ffc88997a2686dfeee6fe5
JimGreenberg <greenjberg@gmail.com>
2017-03-22 19:13:51 -0400

    text fix

--- frontend/util/weather_api_util.js
+++ frontend/util/weather_api_util.js
@@ -4,13 +4,13 @@ export const fetchWeather = query => {
   const type = !!parseInt(query) ? 'id' : 'q';
   return $.getJSON({
     method: "GET",
-    url: `//api.openweathermap.org/data/2.5/weather?${type}=${query}&APPID=${apiKey}&units=imperial`
+    url: `http://api.openweathermap.org/data/2.5/weather?${type}=${query}&APPID=${apiKey}&units=imperial`
   });
 };
 
 export const fetchBatchWeather = query => {
   return $.getJSON({
     method: "GET",
-    url: `//api.openweathermap.org/data/2.5/group?id=${query}&APPID=${apiKey}&units=imperial`
+    url: `http://api.openweathermap.org/data/2.5/group?id=${query}&APPID=${apiKey}&units=imperial`
   });
 };
