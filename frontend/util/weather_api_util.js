d6a129a57c76287925cb4902bae978ab1051cafa
JimGreenberg <greenjberg@gmail.com>
2017-03-22 19:09:36 -0400

    change ajax url

--- frontend/util/weather_api_util.js
+++ frontend/util/weather_api_util.js
@@ -1,16 +1,16 @@
-const apiKey = '0c6887ca19ca753c65c470a739cbd8c6'; // please don't steal
-// const apiKey =  'f816d7f39052e3a98b21952097a43076';
+// const apiKey =  'cd5d3b9fae8549724d78fe69cc25516d';
+const apiKey =  'bcb83c4b54aee8418983c2aff3073b3b';
 export const fetchWeather = query => {
   const type = !!parseInt(query) ? 'id' : 'q';
-  return $.getJSON({
+  return $.ajax({
     method: "GET",
-    url: `https://api.openweathermap.org/data/2.5/weather?${type}=${query}&APPID=${apiKey}&units=imperial`
+    url: `//api.openweathermap.org/data/2.5/weather?${type}=${query}&APPID=${apiKey}&units=imperial`
   });
 };
 
 export const fetchBatchWeather = query => {
-  return $.getJSON({
+  return $.ajax({
     method: "GET",
-    url: `https://api.openweathermap.org/data/2.5/group?id=${query}&APPID=${apiKey}&units=imperial`
+    url: `//api.openweathermap.org/data/2.5/group?id=${query}&APPID=${apiKey}&units=imperial`
   });
 };
